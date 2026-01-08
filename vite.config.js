d3a5ebe6c542b25109b41f1ad146b8758a322310
Zucram <5426869+zucram@users.noreply.github.com>
2026-01-08 15:03:31 +0000

    fix: update base path to P5Tracker in vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/p5r-tactician/', // REQUIRED: Must match your GitHub repository name
+  base: '/P5Tracker/', // REQUIRED: Must match your GitHub repository name
 })
